7e1dbee69703c7f5dd38e465c4b955d384178c5c
nhannt <nhannt.gm@gmail.com>
2025-02-03 15:52:58 +0700

    [add] config nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/public',
+  // basePath: '/ngocanh.tricuong',
   images: {
     unoptimized: true,
-  },
-  experimental: {
-    turbo: {},
   },
 }
 
